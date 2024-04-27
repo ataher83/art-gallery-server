d42e0f3e60929d82e7555e1eb2f98262c233827c
ataher83 <taher83@gmail.com>
2024-04-27 13:57:59 +0600

    CRUD operation updated

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-//theArtGallery
-//nh3zjCRfgP5XXpvT
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0yjrwty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
